448feb0a105173870e98d5e63f08818a265258bd
mariongordo <77761702+mariongordo@users.noreply.github.com>
2021-01-21 11:02:45 +0800

    Update week3day1and2.php

--- week3day1and2.php
+++ week3day1and2.php
@@ -2,33 +2,18 @@
 <H1>
  <title>TODAY</title>
 </H1>
-
 <Body bgcolor = "yellow">
-
 <u>The world is now suffering <br> from corona virus </u>
-
 <br> Let us always pray for our health. <br> <br>
-
 <p align="center"><IMG SRC="C:\Users\kingm\Downloads\week3\sun.jpg" width="210", length="210"></img>
 <br>The sunshine is so bright <br>
-
 It gives hope to everyone <br> <br>
-
-
 <IMG SRC="C:\Users\kingm\Downloads\week3\circle.jfif" width="300", length="300"> </img> <br>
-
 It’s a small <br>
-
 <b>World after all</b> <br> <br> </p>
-
- 
-
 <IMG SRC="C:\Users\kingm\Downloads\week3\smiley.jfif" width="450", length="450"> </img> <p align=”right”>
-
 <b>Always wear a smile,  </b><br> you might inspire somebody <br> with it. </p>
-
 <br><br>
-
 <br><b><i><font color="red" font size="1" font face="verdana">"Beginning today, I will create a new future by creating a new me.  
 <br>
 <br><font color="black" font size="2">No longer will I dwell in a pit of despair,
@@ -50,7 +35,5 @@ It’s a small <br>
 <p align="right"><IMG SRC="C:\Users\kingm\Downloads\week3\littleprince.png" width="210", length="210"></img>
 <br><p align="center"><font color="brown" font size="10", font face="verdana">Marion
 <br><br><br>
-
 </Body>
-
 </html>
